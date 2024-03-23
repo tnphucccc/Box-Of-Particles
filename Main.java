cf02d991e04c2b8cf93764dc79abef2804f4919e
tnphucccc <ktsukasa1412@gmail.com>
2024-03-23 20:14:18 +0700

    feat: ignore interations count

--- Main.java
+++ Main.java
@@ -7,14 +7,14 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             myBox.particlesGenerate(rand.nextInt(myBox.getWidth()), rand.nextInt(myBox.getHeight()));
         }
-        int iterationCounter = 0;
+        //int iterationCounter = 0;
 
         while (!myBox.overload) {
-            System.out.print("\033[H\033[2J");
+            //System.out.print("\033[H\033[2J");
             System.out.flush();
             myBox.visualize();
-            iterationCounter++;
-            System.out.println("Iteration No." + iterationCounter);
+            //iterationCounter++;
+            //System.out.println("Iteration No." + iterationCounter);
             try {
                 Thread.sleep(150);
             } catch (InterruptedException e) {
